cd00411f3d35713e17e265781462255040acbdc3
かみやりお <144749557+rio-honi@users.noreply.github.com>
2024-12-15 12:37:19 +0900

    #3 MySQLとの接続
    MySQLとの接続

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+import os
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"  # フラッシュメッセージ用
+
+#データベースの設定
+def db_config():
+    conn = mysql.connector.connect(
+        host = "127.0.0.1",
+        user = "root",
+        password = "Passw0rd",
+        db = "2024sk",        
+    )
+    return conn
+
+#データベースの接続
+conn = db_config()
+#データベースの操作用変数定義
+cursor = conn.cursor()
+
+sql = "select * from t_users"
+#SQLの実行
+cursor.execute(sql)
+#データの取り出し
+data = cursor.fetchall()
+#出力
+print(data)
+
+#データベース接続の初期化
+#db = SQLAlchemy(app)
 
 #一番初めのページ：index.html
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html" ,data = data)
 
 #ログインページ：login.html
 @app.route('/login')
@@ -14,9 +44,9 @@ def login():
     return render_template("login.html")
 
 #ユーザー登録するページ：register.html
-@app.route('/register')
-def register():
-    return render_template("register.html")
+@app.route('/register', methods=['GET', 'POST'])
+def register():    
+    return render_template('register.html')
 
 #ホーム：main.html
 @app.route('/main')
